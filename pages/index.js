e514f799bdaba6b98f2e9f9817c791e2ec5fec0a
maulikdudhat <maulikdudhat450@gmail.com>
2022-02-28 22:08:50 +0530

    creted-app

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+import Head from 'next/head'
+import Image from 'next/image'
+import Demo from './123'
+
+export default function Home() {
+  return (
+    <Demo />
+  )
+}
---
+++ studio/schemas/schema.js
@@ -0,0 +1,63 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  // We name our schema
+  name: 'default',
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    /* Your types here! */
+
+    {
+      name: 'users',
+      title: 'Users',
+      type: 'documnet',
+      feilds: [
+        {
+          name: 'username',
+          title: 'User Name',
+          type: 'string'
+        },
+        {
+          name: 'WalletAddress',
+          title: 'Wallet Address',
+          type: 'string'
+        },
+        {
+          name: 'ProfileImage',
+          title: 'Profile Image',
+          type: 'image'
+        },
+        {
+          name: 'bannerImage',
+          title: 'Banner Image',
+          type: 'image'
+        },
+        {
+          name: 'twitterHandle',
+          title: 'Twitter Handle',
+          type: 'string'
+        },
+        {
+          name: 'igHandle',
+          title: 'Instagram Handle',
+          type: 'string',
+        }
+      ]
+    },
+    {
+      name: 'marketItems',
+      title: 'Market Items',
+      type: 'document',
+      // feilds: [
+      //   name: 'title'
+
+      // ]
+    }
+  ]),
+})
